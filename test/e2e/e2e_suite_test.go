7f586a15177329c791b771972cc8bf637a89f808
Nicolas Bennett <3158446+nbenn@users.noreply.github.com>
2026-02-03 15:17:40 +0100

    retry fixing coverage in e2e

--- test/e2e/e2e_suite_test.go
+++ test/e2e/e2e_suite_test.go
@@ -33,10 +33,18 @@ import (
 
 var (
 	// managerImage is the manager image to be built and loaded for testing.
+	// Can be overridden via IMG environment variable.
 	managerImage = "example.com/fluxup:v0.0.1"
 	// shouldCleanupCertManager tracks whether CertManager was installed by this suite.
 	shouldCleanupCertManager = false
 )
+
+func init() {
+	// Allow overriding the manager image via environment variable
+	if img := os.Getenv("IMG"); img != "" {
+		managerImage = img
+	}
+}
 
 // TestE2E runs the e2e test suite to validate the solution in an isolated environment.
 // The default setup requires Kind and CertManager.
@@ -49,16 +57,21 @@ func TestE2E(t *testing.T) {
 }
 
 var _ = BeforeSuite(func() {
-	By("building the manager image")
-	cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", managerImage))
-	_, err := utils.Run(cmd)
-	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to build the manager image")
+	// When running with coverage, the Makefile already builds and loads the coverage image
+	if os.Getenv("DEPLOY_COVERAGE") != "true" {
+		By("building the manager image")
+		cmd := exec.Command("make", "docker-build", fmt.Sprintf("IMG=%s", managerImage))
+		_, err := utils.Run(cmd)
+		ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to build the manager image")
 
-	// TODO(user): If you want to change the e2e test vendor from Kind,
-	// ensure the image is built and available, then remove the following block.
-	By("loading the manager image on Kind")
-	err = utils.LoadImageToKindClusterWithName(managerImage)
-	ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to load the manager image into Kind")
+		// TODO(user): If you want to change the e2e test vendor from Kind,
+		// ensure the image is built and available, then remove the following block.
+		By("loading the manager image on Kind")
+		err = utils.LoadImageToKindClusterWithName(managerImage)
+		ExpectWithOffset(1, err).NotTo(HaveOccurred(), "Failed to load the manager image into Kind")
+	} else {
+		By("skipping image build - coverage mode uses pre-built image")
+	}
 
 	setupCertManager()
 })
